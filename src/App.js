4b3839526c8f6101fbb53303273617b01d51bab1
Fa11enSky <iwanpomirsky@gmail.com>
2024-04-11 21:40:52 +0300

    add react-router-dom & pages structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes,Route } from "react-router-dom";
+import Movies from "./pages/Movies";
+import Favorites from "./pages/Favorites";
+import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Movies/>}/>
+          <Route path="favorites" element={<Favorites/>}/>
+        </Route>
+        <Route path="*" element={"<div>not found</div>"}/>
+     </Routes>
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import css from './styles.module.css'
+const Header = () => {
+  return (
+  <header className={css.header}>fadsdfas</header>
+  )
+}
+
+export default Header
