361dd9c89d9e50bf4f233033f2f1bc63804fdecf
Fa11enSky <iwanpomirsky@gmail.com>
2024-04-11 23:41:42 +0300

    header + styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-import css from './styles.module.css'
+import React from "react";
+import css from "./styles.module.css";
+import { NavLink } from "react-router-dom";
 const Header = () => {
   return (
-  <header className={css.header}>fadsdfas</header>
-  )
-}
+    <header className={css.header}>
+      <nav>
+        <ul className={css.navList}>
+          <li>
+            <NavLink className={css.navLink} to="/">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink className={css.navLink} to="/favorites">
+              Favorites
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
