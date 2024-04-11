e166562e9d2482d1b3249153dff4b33405078bcc
Fa11enSky <iwanpomirsky@gmail.com>
2024-04-11 23:42:29 +0300

    add container

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import { Routes,Route } from "react-router-dom";
+import './App.css'
+import { Routes, Route } from "react-router-dom";
 import Movies from "./pages/Movies";
 import Favorites from "./pages/Favorites";
 import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Layout/>}>
-          <Route index element={<Movies/>}/>
-          <Route path="favorites" element={<Favorites/>}/>
-        </Route>
-        <Route path="*" element={"<div>not found</div>"}/>
-     </Routes>
+    <div className='container'>
+      
+        <Routes>
+          <Route path="/" element={<Layout/>}>
+            <Route index element={<Movies/>}/>
+            <Route path="favorites" element={<Favorites/>}/>
+          </Route>
+          <Route path="*" element={"<div>not found</div>"}/>
+       </Routes>
     </div>
+    
   );
 }
 
