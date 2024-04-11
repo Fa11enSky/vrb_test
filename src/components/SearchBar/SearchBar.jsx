33c822462e677cdd742c6c7583bc973129ddaeb3
Fa11enSky <iwanpomirsky@gmail.com>
2024-04-12 02:53:24 +0300

    update styles searchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-
+import css from './styles.module.css'
 const SearchBar = ({ setQuery }) => {
   const handleInput = (e) => {
     setQuery(e.target.value);
   };
 
   return (
-    <div>
+    <label className={css.inputWrapper}>
       <input onChange={handleInput} type="text" />
-    </div>
+    </label>
   );
 };
 
