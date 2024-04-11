a097c1df0a96e266b8c50015f2b21b430b9d55b7
Fa11enSky <iwanpomirsky@gmail.com>
2024-04-11 23:43:24 +0300

    movie list/styles

---
+++ src/components/MoviesList/MoviesList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ListItem from '../ListItem/ListItem'
+import css from './styles.module.css'
+const MoviesList = ({data}) => {
+  return (
+    <ul className={css.movieList}>
+     {data.map(el=><ListItem key={el.id} item={el}/>)} 
+    </ul>
+  )
+}
+
+export default MoviesList
