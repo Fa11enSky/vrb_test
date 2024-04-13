e6146b37f05499028f1355489785b5f34009d7f1
Fa11enSky <iwanpomirsky@gmail.com>
2024-04-13 03:18:19 +0300

    render btn to add movie

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -4,21 +4,23 @@ import MoviesList from "../components/MoviesList/MoviesList";
 import SearchBar from "../components/SearchBar/SearchBar";
 import filterByTitle from "../helpers/filterByTitle";
 import { useMoviesContext } from "../components/MoviesContext/MoviesContext";
+import AddBtn from "../components/AddBtn/AddBtn";
+import { Notify } from "notiflix";
 
 const Movies = () => {
   const { movies, setMovies } = useMoviesContext();
-  const [error, setError] = useState(null);
   const [query, setQuery] = useState("");
 
   useEffect(() => {
     fetchAllMovies()
       .then((data) => setMovies(data))
-      .catch((e) => setError(e));
+      .catch((e) => Notify.failure(e.message));
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <section>
-      <SearchBar setQuery={setQuery} />
+     <div> <SearchBar setQuery={setQuery} /> <AddBtn/></div>
       <MoviesList data={query ? filterByTitle(query, movies) : movies} />
     </section>
   );
