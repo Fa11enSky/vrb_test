e8b79725de1158deaed353c3b6939f21fc92f730
Fa11enSky <iwanpomirsky@gmail.com>
2024-04-13 03:18:06 +0300

    add notify messages to add to favorites operation

--- src/components/MoviesContext/MoviesContext.js
+++ src/components/MoviesContext/MoviesContext.js
@@ -1,26 +1,32 @@
 import React, { createContext, useContext, useState } from "react";
 import deleteMovie from "../../services/deleteMovie";
+import { Notify } from "notiflix";
 
 const MovieContext = createContext();
 
 export const MoviesProvider = ({ children }) => {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
   const [favorites, setFavorites] = useState(
     JSON.parse(window.localStorage.getItem("favorites")) || []
   );
 
   const permanentDelete = (id) => {
-    deleteMovie(id).then((data) => {
-      const updatedFavorites = favorites.filter((el) => el._id !== data._id);
-      const updatedMovies = movies.filter((el) => el._id !== data._id);
-      setMovies(updatedMovies)
-      window.localStorage.setItem(
-        "favorites",
-        JSON.stringify(updatedFavorites)
-      );
+    try {
+      deleteMovie(id).then((data) => {
+        const updatedFavorites = favorites.filter((el) => el._id !== data._id);
+        const updatedMovies = movies.filter((el) => el._id !== data._id);
+        setMovies(updatedMovies);
+        window.localStorage.setItem(
+          "favorites",
+          JSON.stringify(updatedFavorites)
+        );
 
-      setFavorites(updatedFavorites);
-    });
+        setFavorites(updatedFavorites);
+        Notify.success("Movie removed successfully");
+      });
+    } catch (error) {
+      Notify.failure(error.message);
+    }
   };
 
   const toggleFavorites = (movie) => {
@@ -35,6 +41,7 @@ export const MoviesProvider = ({ children }) => {
       );
 
       setFavorites(updatedFavorites);
+      Notify.info("The movie has been successfully removed from favorites");
     } else {
       const updatedFavorites = [...favorites, movie];
 
@@ -44,12 +51,20 @@ export const MoviesProvider = ({ children }) => {
       );
 
       setFavorites(updatedFavorites);
+      Notify.info("The movie has been successfully added to favorites");
     }
   };
 
   return (
     <MovieContext.Provider
-      value={{movies,setMovies, favorites, toggleFavorites, permanentDelete }}
+      value={{
+        movies,
+        setMovies,
+        favorites,
+        setFavorites,
+        toggleFavorites,
+        permanentDelete,
+      }}
     >
       {children}
     </MovieContext.Provider>
